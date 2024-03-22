fa42de4a923175e0650f21be0555f6de91874d3b
sooraj <franksooraj@gmail.com>
2024-03-22 12:13:48 +0530

    home screen boiler plate

--- src/screen/home/home.jsx
+++ src/screen/home/home.jsx
@@ -1,5 +1,27 @@
+import './home.css'
+import { dSlider, DHitGames } from '../../utils/dummyData'
+import { useState } from 'react'
+
 const Home = () => {
-    return "Home"
+    const [sliderData, setSliderData] = useState(dSlider)
+
+    function component1() {
+        return <div className='_1-card'></div>
+    }
+
+    function component2() {
+        return <div className='_2-card'></div>
+    }
+
+    function component3() {
+        return <div className='_2-card'></div>
+    }
+
+    return <>
+        {component1()}
+        {component2()}
+        {component3()}
+    </>
 }
 
 export default Home
---
+++ src/utils/dummyData.js
@@ -0,0 +1,12 @@
+const dSlider = {
+
+}
+
+const dHitGames = {
+
+}
+
+module.exports = {
+    dSlider: dSlider,
+    DHitGames: dHitGames,
+}
