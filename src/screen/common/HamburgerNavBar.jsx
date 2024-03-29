2769570ecbde3da263d0676b4bfebd393f5ef039
tamoghna-10 <137286113+tamoghna-10@users.noreply.github.com>
2024-03-29 16:41:56 +0530

    hamburger navbar made

--- src/screen/common/HamburgerNavBar.jsx
+++ src/screen/common/HamburgerNavBar.jsx
@@ -1,21 +1,78 @@
-import React from 'react';
-import { FaHome, FaPlay } from 'react-icons/fa';
-import { BsThreeDots } from 'react-icons/bs';
+import React, { useState } from "react";
+import { FaHome, FaPlay, FaBars, FaHeart } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 const HamburgerNavBar = () => {
-  return (
-    <div
-      style={{ borderTopLeftRadius: '15px', borderTopRightRadius: '15px' }}
-    >
-    <div
-      className="row fixed-bottom navbar-light bg-dark d-md-none p-2 m justify-content-between"
-    >
-      <div className="col-3 text-center text-white"><FaHome/></div>
-      <div className="col-3 text-center text-white">opt2</div>
-      <div className="col-3 text-center text-white">opt3</div>
-      <div className="col-3 text-center text-white">opt4</div>
-    </div>
+  const [activeIcon, setActiveIcon] = useState(null);
 
+  const navigate = useNavigate();
+
+  const handleIconHover = (icon) => {
+    setActiveIcon(icon);
+    navigate("/");
+  };
+
+  return (
+    <div className="container-fluid">
+      <div
+        className="navbar fixed-bottom navbar-light d-lg-none "
+        style={{
+          borderTopLeftRadius: "15px",
+          borderTopRightRadius: "15px",
+          background: "#25616a",
+        }}
+      >
+        <div
+          className={`col-3 col-md text-center text-white  ${
+            activeIcon === "home" ? "active-icon" : ""
+          }`}
+          title="Home"
+          onMouseEnter={() => handleIconHover("home")}
+        >
+          <div>
+            <FaHome />
+          </div>
+          {activeIcon === "home" && <div className="hover-text ">Home</div>}
+        </div>
+        <div
+          className={`col-3 col-md text-center text-white ${
+            activeIcon === "play" ? "active-icon" : ""
+          }`}
+          title="Video Play"
+          onMouseEnter={() => handleIconHover("play")}
+        >
+          <div>
+            <FaPlay />
+          </div>
+          {activeIcon === "play" && <div className="hover-text">In-Play</div>}
+        </div>
+        <div
+          className={`col-3 col-md text-center text-white ${
+            activeIcon === "heart" ? "active-icon" : ""
+          }`}
+          title="Animated Round Card (Mini Game)"
+          onMouseEnter={() => handleIconHover("heart")}
+        >
+          <div className="animated-coin">
+            <FaHeart />
+          </div>
+          {activeIcon === "heart" && (
+            <div className="hover-text">Mini Game</div>
+          )}
+        </div>
+        <div
+          className={`col-3 col-md text-center text-white ${
+            activeIcon === "menu" ? "active-icon" : ""
+          }`}
+          title="Menu"
+          onMouseEnter={() => handleIconHover("menu")}
+        >
+          <div>
+            <FaBars />
+          </div>
+          {activeIcon === "menu" && <div className="hover-text">Menu</div>}
+        </div>
+      </div>
     </div>
   );
 };
