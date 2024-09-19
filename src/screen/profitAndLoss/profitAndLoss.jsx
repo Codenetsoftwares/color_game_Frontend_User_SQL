1f100e058827cc0ad2c83d50ede2c4c3e2c9f2c7
AmitpaulCNS <paul.amit.cns@gmail.com>
2024-09-19 21:49:59 +0530

    remove loading from profit and loss runner component

--- src/screen/profitAndLoss/profitAndLoss.jsx
+++ src/screen/profitAndLoss/profitAndLoss.jsx
@@ -128,196 +128,195 @@ const ProfitAndLoss = () => {
     return () => clearTimeout(timer);
   }, [profitLossData.searchItem]);
 
- const ProfitAndLossEvent = ({
-   data,
-   SetComponent,
-   SetMarketId,
-   SetProfitLossEventData,
-   currentPage,
-   SetToggle,
-   totalItems,
- }) => {
-   console.log("data", data);
+  const ProfitAndLossEvent = ({
+    data,
+    SetComponent,
+    SetMarketId,
+    SetProfitLossEventData,
+    currentPage,
+    SetToggle,
+    totalItems,
+  }) => {
+    console.log("data", data);
 
-   const startIndex = (currentPage - 1) * data.itemPerPage + 1;
-   const endIndex = Math.min(currentPage * data.itemPerPage, data.totalData);
+    const startIndex = (currentPage - 1) * data.itemPerPage + 1;
+    const endIndex = Math.min(currentPage * data.itemPerPage, data.totalData);
 
-   const handleGotoRunnerWiseProfitLoss = (marketId, componentName) => {
-     SetComponent(componentName);
-     SetMarketId(marketId);
-   };
+    const handleGotoRunnerWiseProfitLoss = (marketId, componentName) => {
+      SetComponent(componentName);
+      SetMarketId(marketId);
+    };
 
-   const handleItemPerPage = (event) => {
-     console.log("event.target.value", event.target.value);
-     SetProfitLossEventData((prevState) => ({
-       ...prevState,
-       itemPerPage: Number(event.target.value),
-       currentPage: Number(currentPage),
-     }));
-     toast.error("Work Pending From ServerSide");
-   };
+    const handleItemPerPage = (event) => {
+      console.log("event.target.value", event.target.value);
+      SetProfitLossEventData((prevState) => ({
+        ...prevState,
+        itemPerPage: Number(event.target.value),
+        currentPage: Number(currentPage),
+      }));
+      toast.error("Work Pending From ServerSide");
+    };
 
-   const handleSearch = (e) => {
-     SetProfitLossEventData((prev) => ({
-       ...prev,
-       searchItem: e.target.value,
-     }));
-   };
+    const handleSearch = (e) => {
+      SetProfitLossEventData((prev) => ({
+        ...prev,
+        searchItem: e.target.value,
+      }));
+    };
 
-   return (
-     <>
-       {/* card */}
-       <div className="card w-100 rounded">
-         <div
-           className="card-heade text-white p-1 d-flex justify-content-between"
-           style={{ backgroundColor: "#2CB3D1" }}
-         >
-           <b>&nbsp;&nbsp;Profit & Loss Events</b>
-           <span
-             style={{ cursor: "pointer" }}
-             title="Back"
-             onClick={() => {
-               SetToggle(true);
-             }}
-           >
-             <i className="fas fa-arrow-left"></i>
-           </span>
-         </div>
-         <div className="m-1 d-flex justify-content-between align-items-center">
-           <select
-             className="form-select w-auto m-1"
-             onChange={handleItemPerPage}
-           >
-             <option value="10" selected={data.itemPerPage === 10}>
-               10 Entries
-             </option>
-             <option value="25" selected={data.itemPerPage === 25}>
-               25 Entries
-             </option>
-             <option value="50" selected={data.itemPerPage === 50}>
-               50 Entries
-             </option>
-             <option value="100" selected={data.itemPerPage === 100}>
-               100 Entries
-             </option>
-           </select>
-           <input
-             type="search"
-             className="form-control w-auto"
-             placeholder="Search..."
-             onChange={handleSearch}
-           />
-         </div>
-         <ul className="list-group list-group-flush">
-           <li className="list-group-item">
-             <div className="white_card_body">
-               {data?.data?.length === 0 && totalItems !== 0 ? (
-                 // Loader
-                 <div
-                   className="d-flex justify-content-center align-items-center"
-                   style={{ height: "100px" }}
-                 >
-                   <div className="spinner-border" role="status">
-                     <span className="sr-only">Loading...</span>
-                   </div>
-                 </div>
-               ) : (
-                 // Table
-                 <div className="QA_section">
-                   <div className="QA_table mb_30">
-                     <table className="table lms_table_active3 table-bordered">
-                       <thead>
-                         <tr
-                           style={{
-                             backgroundColor: "#e6e9ed",
-                             color: "#5562a3",
-                           }}
-                           align="center"
-                         >
-                           <th scope="col">
-                             <b>Sport Name</b>
-                           </th>
-                           <th scope="col">
-                             <b>Event Name</b>
-                           </th>
-                           <th scope="col">
-                             <b>Commission</b>
-                           </th>
-                           <th scope="col">
-                             <b>Profit & Loss</b>
-                           </th>
-                           <th scope="col">
-                             <b>Total P&L</b>
-                           </th>
-                         </tr>
-                         {data?.data?.length > 0 ? (
-                           data?.data?.map((data, index) => (
-                             <tr key={index} align="center">
-                               <td>{data?.gameName}</td>
-                               <td
-                                 className="text-primary fw-bold"
-                                 style={{ cursor: "pointer" }}
-                                 onClick={() => {
-                                   handleGotoRunnerWiseProfitLoss(
-                                     data.marketId,
-                                     "ProfitAndLossRunner"
-                                   );
-                                 }}
-                               >
-                                 {data?.marketName}
-                               </td>
-                               <td>{data?.commission || "NDS"}</td>
-                               <td>{data?.profitLoss || "NDS"}</td>
-                               <td
-                                 className={`fw-bold ${
-                                   data?.totalProfitLoss > 0
-                                     ? "text-success"
-                                     : "text-danger"
-                                 }`}
-                               >
-                                 {data?.totalProfitLoss}
-                               </td>
-                             </tr>
-                           ))
-                         ) : (
-                           <tr align="center">
-                             <td colSpan="5">
-                               <div
-                                 className="alert alert-info fw-bold"
-                                 role="alert"
-                               >
-                                 No Data Found !!
-                               </div>
-                             </td>
-                           </tr>
-                         )}
-                       </thead>
-                     </table>
-                   </div>
-                 </div>
-               )}
-             </div>
-           </li>
-           <li className="list-group-item">
-             {/* Pagination */}
-             {data?.data?.length > 0 && (
-               <Pagination
-                 currentPage={data.currentPage}
-                 totalPages={data.totalPages}
-                 handlePageChange={data.handlePageChange}
-                 startIndex={startIndex}
-                 endIndex={endIndex}
-                 totalData={data.totalData}
-               />
-             )}
-             {/* Pagination */}
-           </li>
-         </ul>
-       </div>
-     </>
-   );
- };
-
+    return (
+      <>
+        {/* card */}
+        <div className="card w-100 rounded">
+          <div
+            className="card-heade text-white p-1 d-flex justify-content-between"
+            style={{ backgroundColor: "#2CB3D1" }}
+          >
+            <b>&nbsp;&nbsp;Profit & Loss Events</b>
+            <span
+              style={{ cursor: "pointer" }}
+              title="Back"
+              onClick={() => {
+                SetToggle(true);
+              }}
+            >
+              <i className="fas fa-arrow-left"></i>
+            </span>
+          </div>
+          <div className="m-1 d-flex justify-content-between align-items-center">
+            <select
+              className="form-select w-auto m-1"
+              onChange={handleItemPerPage}
+            >
+              <option value="10" selected={data.itemPerPage === 10}>
+                10 Entries
+              </option>
+              <option value="25" selected={data.itemPerPage === 25}>
+                25 Entries
+              </option>
+              <option value="50" selected={data.itemPerPage === 50}>
+                50 Entries
+              </option>
+              <option value="100" selected={data.itemPerPage === 100}>
+                100 Entries
+              </option>
+            </select>
+            <input
+              type="search"
+              className="form-control w-auto"
+              placeholder="Search..."
+              onChange={handleSearch}
+            />
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <div className="white_card_body">
+                {data?.data?.length === 0 && totalItems !== 0 ? (
+                  // Loader
+                  <div
+                    className="d-flex justify-content-center align-items-center"
+                    style={{ height: "100px" }}
+                  >
+                    <div className="spinner-border" role="status">
+                      <span className="sr-only">Loading...</span>
+                    </div>
+                  </div>
+                ) : (
+                  // Table
+                  <div className="QA_section">
+                    <div className="QA_table mb_30">
+                      <table className="table lms_table_active3 table-bordered">
+                        <thead>
+                          <tr
+                            style={{
+                              backgroundColor: "#e6e9ed",
+                              color: "#5562a3",
+                            }}
+                            align="center"
+                          >
+                            <th scope="col">
+                              <b>Sport Name</b>
+                            </th>
+                            <th scope="col">
+                              <b>Event Name</b>
+                            </th>
+                            <th scope="col">
+                              <b>Commission</b>
+                            </th>
+                            <th scope="col">
+                              <b>Profit & Loss</b>
+                            </th>
+                            <th scope="col">
+                              <b>Total P&L</b>
+                            </th>
+                          </tr>
+                          {data?.data?.length > 0 ? (
+                            data?.data?.map((data, index) => (
+                              <tr key={index} align="center">
+                                <td>{data?.gameName}</td>
+                                <td
+                                  className="text-primary fw-bold"
+                                  style={{ cursor: "pointer" }}
+                                  onClick={() => {
+                                    handleGotoRunnerWiseProfitLoss(
+                                      data.marketId,
+                                      "ProfitAndLossRunner"
+                                    );
+                                  }}
+                                >
+                                  {data?.marketName}
+                                </td>
+                                <td>{data?.commission || "NDS"}</td>
+                                <td>{data?.profitLoss || "NDS"}</td>
+                                <td
+                                  className={`fw-bold ${
+                                    data?.totalProfitLoss > 0
+                                      ? "text-success"
+                                      : "text-danger"
+                                  }`}
+                                >
+                                  {data?.totalProfitLoss}
+                                </td>
+                              </tr>
+                            ))
+                          ) : (
+                            <tr align="center">
+                              <td colSpan="5">
+                                <div
+                                  className="alert alert-info fw-bold"
+                                  role="alert"
+                                >
+                                  No Data Found !!
+                                </div>
+                              </td>
+                            </tr>
+                          )}
+                        </thead>
+                      </table>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </li>
+            <li className="list-group-item">
+              {/* Pagination */}
+              {data?.data?.length > 0 && (
+                <Pagination
+                  currentPage={data.currentPage}
+                  totalPages={data.totalPages}
+                  handlePageChange={data.handlePageChange}
+                  startIndex={startIndex}
+                  endIndex={endIndex}
+                  totalData={data.totalData}
+                />
+              )}
+              {/* Pagination */}
+            </li>
+          </ul>
+        </div>
+      </>
+    );
+  };
 
   const ProfitAndLossRunner = ({
     data,
@@ -397,7 +396,7 @@ const ProfitAndLoss = () => {
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <div className="white_card_body">
-                {data?.data?.length === 0 && !loading ? (
+                {data?.data?.length === 0 ? (
                   // Loader
                   <div
                     className="d-flex justify-content-center align-items-center"
@@ -513,7 +512,6 @@ const ProfitAndLoss = () => {
       </>
     );
   };
-
 
   const ProfitAndLoss = ({
     setEndDate,
