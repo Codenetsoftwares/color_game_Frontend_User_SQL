edf55fedb835becf052a6ee8e443ac1f35f0d0af
AmitpaulCNS <paul.amit.cns@gmail.com>
2024-09-24 11:10:24 +0530

    add void,back,lay button and small color ui change on userBethistory component for p&l and remove console

--- src/screen/profitAndLoss/UserBetHistory.js
+++ src/screen/profitAndLoss/UserBetHistory.js
@@ -1,40 +1,40 @@
-import React from 'react'
+import React from 'react';
 
 const UserBetHistory = ({ data, SetComponent }) => {
     return (
         <div className="card w-100 rounded">
+            {/* Header section with title and back button */}
             <div
-                className="card-heade text-white p-1 d-flex justify-content-between"
+                className="card-header text-white p-1 d-flex justify-content-between" 
                 style={{ backgroundColor: "#2CB3D1" }}
             >
                 <b>&nbsp;&nbsp;Bet History</b>
                 <span
-                    style={{ cursor: "pointer" }}
+                    style={{ cursor: "pointer" }} 
                     title="Back"
                     onClick={() => {
-                        SetComponent("ProfitAndLossRunner");
+                        SetComponent("ProfitAndLossRunner"); 
                     }}
                 >
-                    {" "}
-                    <i className="fas fa-arrow-left"></i>
+                    <i className="fas fa-arrow-left"></i> 
                 </span>
             </div>
+
+            {/* Button section for filtering bets (Back, Lay, Void) */}
             <div className="m-1 d-flex justify-content-end align-items-end">
-
-                <input
-                    type="search"
-                    className="form-control w-auto"
-                    placeholder="Search..."
-
-                />
+                <button style={{ margin: "4px", backgroundColor: "lightBlue" }}>Back</button>
+                <button style={{ margin: "4px", backgroundColor: "lightPink" }}>Lay</button>
+                <button style={{ margin: "4px", backgroundColor: "white" }}>Void</button>
             </div>
 
+            {/* Main table section that displays bet history */}
             <ul className="list-group list-group-flush" style={{ overflowX: "auto" }}>
                 <li className="list-group-item">
                     <div className="QA_section">
                         <div className="QA_table mb_30" style={{ overflowX: "auto" }}>
                             <table className="table lms_table_active3 table-bordered">
                                 <thead>
+                                    {/* Table header */}
                                     <tr
                                         style={{
                                             backgroundColor: "#e6e9ed",
@@ -42,85 +42,43 @@ const UserBetHistory = ({ data, SetComponent }) => {
                                         }}
                                         align="center"
                                     >
-                                        <th scope="col">
-                                            <b>Sport Name</b>
-                                        </th>
-                                        <th scope="col">
-                                            <b>Event Name</b>
-                                        </th>
-                                        <th scope="col">
-                                            <b>Market Name</b>
-                                        </th>
-                                        <th scope="col">
-                                            <b>Selection Name</b>
-                                        </th>
-                                        <th scope="col">
-                                            <b>Bet Type</b>
-                                        </th>
-                                        <th scope="col">
-                                            <b>User Price</b>
-                                        </th>
-                                        <th scope="col">
-                                            <b>Amount</b>
-                                        </th>
-                                        <th scope="col">
-                                            <b>Profit/Loss</b>
-                                        </th>
-                                        <th scope="col">
-                                            <b>Place Date</b>
-                                        </th>
-                                        <th scope="col">
-                                            <b>Match Date</b>
-                                        </th>
+                                        <th scope="col"><b>Sport Name</b></th>
+                                        <th scope="col"><b>Event Name</b></th>
+                                        <th scope="col"><b>Market Name</b></th>
+                                        <th scope="col"><b>Selection Name</b></th>
+                                        <th scope="col"><b>Bet Type</b></th>
+                                        <th scope="col"><b>User Price</b></th>
+                                        <th scope="col"><b>Amount</b></th>
+                                        <th scope="col"><b>Profit/Loss</b></th>
+                                        <th scope="col"><b>Place Date</b></th>
+                                        <th scope="col"><b>Match Date</b></th>
                                     </tr>
+                                    {/* Render bet history data if available */}
                                     {data?.data?.length > 0 ? (
                                         data?.data?.map((data, index) => (
                                             <tr key={index} align="center">
-                                                <td>{data?.gameName}</td>
-                                                <td
-                                                    className="text-primary fw-bold"
-                                                    style={{ cursor: "pointer" }}
-                                                    onClick={() => {
-                                                        handelGotoRunnerWiseProfitLoss(
-                                                            data.marketId,
-                                                            "ProfitAndLossRunner"
-                                                        );
-                                                    }}
-                                                >
+                                                <td>{data?.gameName}</td> 
+                                                <td className="text-primary fw-bold" style={{ cursor: "pointer" }}>
                                                     {data?.marketName}
                                                 </td>
-                                                <td>{data?.commission || 0}</td>
-                                                <td
-                                                    className={`fw-bold ${data?.totalProfitLoss > 0
-                                                        ? "text-success"
-                                                        : "text-danger"
-                                                        }`}
-                                                >
-                                                    {data?.totalProfitLoss}
+                                                <td>{"Winner"}</td> 
+                                                <td>{data?.runnerName}</td> 
+                                                <td>{data?.type}</td> 
+                                                <td>{data?.rate}</td> 
+                                                <td>{data?.value}</td> 
+                                                <td>
+                                                    <span className='text-success mx-1'>{data?.bidAmount}</span>
+                                                    <span className='text-danger'>(-{data?.value})</span>
                                                 </td>
-                                                <td
-                                                    className={`fw-bold ${data?.totalProfitLoss > 0
-                                                        ? "text-success"
-                                                        : "text-danger"
-                                                        }`}
-                                                >
-                                                    {data?.totalProfitLoss}
-                                                </td>
-                                                <td>{data?.gameName}</td>
-                                                <td>{data?.gameName}</td>
-                                                <td>{data?.gameName}</td>
-                                                <td>{data?.gameName}</td>
-                                                <td>{data?.gameName}</td>
-
+                                                <td>{data?.placeDate}</td> 
+                                                <td>{data?.matchDate}</td> 
                                             </tr>
                                         ))
                                     ) : (
+                                        // Display message if no data is available
                                         <tr align="center">
-                                            <td colspan="10">
-                                                <div
-                                                    class="alert alert-info fw-bold"
-                                                    role="alert"
-                                                >
+                                            <td colSpan="10"> {/* Fixed typo from colspan="10" to colSpan="10" */}
+                                                <div className="alert alert-info fw-bold" role="alert">
                                                     No Data Found !!
                                                 </div>
                                             </td>
@@ -131,10 +89,9 @@ const UserBetHistory = ({ data, SetComponent }) => {
                         </div>
                     </div>
                 </li>
-
             </ul>
         </div>
-    )
-}
+    );
+};
 
-export default UserBetHistory
+export default UserBetHistory;
