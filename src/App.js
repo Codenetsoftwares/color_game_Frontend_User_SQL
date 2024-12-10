5b9a5054bd8abd55dfed190ad51c34e3606ab9cc
AmitpaulCNS <paul.amit.cns@gmail.com>
2024-12-10 16:41:10 +0530

    toastify postion center and autoclose time to reduce 500ms and chnage the url to production

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ function App() {
   return (
     <AppProvider>
       <ToastContainer
-        position="top-right"
-        autoClose={3000}
+        position="top-center"
+        autoClose={500}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
